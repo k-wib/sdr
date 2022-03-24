6781c9b26060e6782b08c3496294ac16d25d5356
Felipe Maia Polo <felipemaiapolo@gmail.com>
2022-03-24 12:05:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['numpy','sklearn','matplotlib','pytorch==1.7.1']
+    install_requires=['numpy','sklearn','matplotlib','pytorch==1.0.2']
 ) 
